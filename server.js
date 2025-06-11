3c79516e211e2ef53acf82c6c4a886ee31e85940
manan patel <131731623+tejanimanan@users.noreply.github.com>
2025-06-11 11:51:40 +0530

    Create server.js
    new

---
+++ server.js
@@ -0,0 +1,16 @@
+// server.js
+import jsonServer from 'json-server';
+import cors from 'cors';
+
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(cors());
+server.use(middlewares);
+server.use(router);
+
+const PORT = process.env.PORT || 5001;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on http://localhost:${PORT}`);
+});
